b99c96c7c1f1bb646736e06827f5e944fd75a1b5
Steven Barre <steven@stevenbarre.com>
2018-01-28 19:06:20 -0800

    Old progress

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import bottle
 import os
+import sys
 import random
-
+from pprint import pprint
+from AStar import *
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -23,10 +25,12 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': '#%06X' % random.randint(0,256**3-1),
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
-        'head_url': head_url,
-        'name': 'battlesnake-python'
+        'head_url': 'https://cn.pling.com/img//hive/content-pre1/104822-1.png',
+        'name': 'Winding Constrictor %i' % random.randint(0,255),
+	'head_type': 'tongue',
+	'tail_type': 'freckled',
     }
 
 
@@ -37,11 +41,58 @@ def move():
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
 
+    mysnek, grid = init(data)
+
+    myhead = mysnek['coords'][0]
+    
+    # Eliminate completly unsafe directions
+    # Walls, and other snakes
+    for direction in directions[:]:
+        x = myhead[0]
+        y = myhead[1]
+        if direction == 'up':
+            y -= 1
+        if direction == 'down':
+            y += 1
+        if direction == 'left':
+            x -= 1
+        if direction == 'right':
+            x +=1
+
+        if x < 0 or x >= data['width']:
+            directions.remove(direction)
+            continue
+        if y < 0 or y >= data['height']:
+            directions.remove(direction)
+            continue
+
+        if grid[x][y] == 'B':
+	    directions.remove(direction)
+            continue
+
+    #pprint (data)
+    #pprint (zip(*grid), width=120)
+    #pprint (directions)
+
+    move = random.choice(directions)
+    #print move
     return {
-        'move': random.choice(directions),
+        'move': move,
         'taunt': 'battlesnake-python!'
     }
 
+def init(data):
+    grid = [['E' for col in xrange(data['height'])] for row in xrange(data['width'])]
+    for snek in data['snakes']:
+        if snek['id']== data['you']:
+            mysnek = snek
+        for coord in snek['coords']:
+            grid[coord[0]][coord[1]] = 'B'
+
+    for food in data['food']:
+        grid[food[0]][food[1]] = 'F'
+    
+    return mysnek, grid
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
